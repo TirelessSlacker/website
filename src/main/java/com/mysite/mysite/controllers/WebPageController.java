9fb2215e17f3df2f380b49e42a9456db5b7e2ed3
Alex <dgrabbe12@gmail.com>
2022-11-23 18:47:26 -0500

    setting default mappings

--- src/main/java/com/mysite/mysite/controllers/WebPageController.java
+++ src/main/java/com/mysite/mysite/controllers/WebPageController.java
@@ -2,21 +2,21 @@ package com.mysite.mysite.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.mysite.mysite.constants.templateData.BlogPostArchives;
 import com.mysite.mysite.data.BlogPosts;
 
 @Controller
 public class WebPageController {
 
-    @GetMapping(value = "/") 
+    @RequestMapping(value = "/") 
     public String home(Model model) {
         model.addAttribute("blogPostArchive",BlogPostArchives.HOME_PAGE);
         model.addAttribute("blogPosts",BlogPosts.getReverseChronological());
         return  "blogPostArchive";
     }
 
-    @GetMapping(value = "/fromBeginning")
+    @RequestMapping(value = "/fromBeginning")
     public String fromBeginning(Model model) {
         model.addAttribute("blogPostArchive",BlogPostArchives.CHRONOLOGICAL_ORDERING);
         model.addAttribute("blogPosts",BlogPosts.getChronological());
