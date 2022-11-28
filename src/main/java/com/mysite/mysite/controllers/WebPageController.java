377ae69c22e68b8b7f1e5c16cdb919207a31620b
Alex <dgrabbe12@gmail.com>
2022-11-28 17:35:01 -0500

    fixing repo

--- src/main/java/com/mysite/mysite/controllers/WebPageController.java
+++ src/main/java/com/mysite/mysite/controllers/WebPageController.java
@@ -1,7 +1,6 @@
 package com.mysite.mysite.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.mysite.mysite.constants.templateData.BlogPostArchives;
 import com.mysite.mysite.data.BlogPosts;
