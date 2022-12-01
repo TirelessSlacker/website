ed8389cfa14ba5753914a1c23857969e7ff57ec8
Alex <dgrabbe12@gmail.com>
2022-12-01 09:14:48 -0500

    messing with entities

---
+++ src/main/java/com/mysite/mysite/entities/BlogPosts.java
@@ -0,0 +1,60 @@
+package com.mysite.mysite.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BLOG_POSTS")
+public class BlogPosts {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDateTime dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "BODY")
+    private String body;
+
+    @Column(name = "DATE_ADDED")
+    private LocalDateTime dateAdded;
+}
